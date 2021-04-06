c4879bc17d267eb1a2025c2f5320f1a349a54d98
Evan Merlock <vnmrlck@gmail.com>
2021-04-06 11:15:51 -0500

    Add(token): Beginning of lexer.

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,7 +1,55 @@
 use std::io::prelude::*;
+use std::io;
 
 mod token;
 
-trait Lexer {
-    fn lex<'a>(file: &'a dyn Read) -> Vec<token::Token>;
+pub trait Lexer {
+    fn lex<'a>(self, file: &'a mut dyn Read) -> Result<Vec<token::Token>, io::Error>;
+}
+
+pub struct BasicLexer {
+    start: usize,
+    current: usize,
+    line_num: usize,
+}
+
+impl BasicLexer {
+    pub fn new() -> BasicLexer {
+        BasicLexer {
+            start: 0,
+            current: 0,
+            line_num: 0,
+        }
+    }
+}
+
+impl Lexer for BasicLexer {
+    fn lex<'a>(mut self, file: &'a mut dyn Read) -> Result<Vec<token::Token>, io::Error> {
+
+        let mut tokens = Vec::new();
+
+        let mut source = String::new();
+        file.read_to_string(&mut source)?;
+
+        let mut chars = source.chars().peekable();
+
+        let is_at_end = || {
+            return self.current >= source.len()
+        };
+
+        let advance = || {
+            self.current += 1;
+            return chars.next();
+        };
+
+        let add_token = |tok: token::TokenType| {
+            tokens.push(token::Token::new(self.line_num, self.start, self.current, tok));
+        };
+
+        let scan_token = || {
+
+        };
+
+        Ok(tokens)
+    }
 }
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,12 +1,25 @@
 pub struct Token(LocationData, TokenType);
 
+impl Token {
+    pub fn new(line_num: usize, begin: usize, end: usize, typ: TokenType) -> Token {
+        Token (
+            LocationData {
+                line_num: line_num,
+                tok_begin: begin,
+                tok_end: end
+            },
+            typ
+        )
+    }
+}
+
 struct LocationData {
     line_num: usize,
     tok_begin: usize,
     tok_end: usize
 }
 
-enum TokenType {
+pub enum TokenType {
     LeftParen, RightParen, LeftBrace, RightBrace,
     Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
     Colon,
